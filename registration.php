31837a3638624d04474dfb4c9032ce616df7d33c
Vitor Mattos <vitor.mattos@phprio.org>
2014-03-15 00:05:18 -0300

    Delete registration.php
    Sem uso

--- registration.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$db = new SQLiteDatabase('db.sqlite');
-$db->queryExec('insert registry (name, type, rg ,birthdate, enterprise_name, city, state, email, phone) value ("Robson", 0, "21121212", "1988-02-05", "HU", "Rio de Janeiro", "RJ", "robsonvnt@gmail.com", "3232323");');
-echo "test";
